92bb2d7c14c656910de9e105e57e294895ed0b63
tangtao <rache008@163.com>
2017-03-15 14:23:20 +0800

    完成了优酷页面网页的爬虫，但是筛选还是有些许问题，会出现重复的部分

--- youku/youku_spider.py
+++ youku/youku_spider.py
@@ -31,31 +31,57 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-def get_youku_href():
+def get_youku_href(channel_list_addr):
     """
     获取优酷的剧集的信息
     :return:
     """
+    length = len(channel_list_addr)
+    i = 0
+    while i < length:
+        url = channel_list_addr[i]
+        # print url
+        page = urllib2.urlopen(url)
+        soup = BeautifulSoup(page, "html5lib")
+        for info in soup.find_all("a", attrs={'target': "video"}):
+            title = info.get('title')
+            if title is None:
+                pass
+            else:
+                href = info.get('href')
+                # print title + "   " + href
+                data = {
+                    u'标题': title,
+                    u"网址": href,
+                }
+                with open('youku.txt', "a+") as f:
+                    f.write('\n')
+                    json.dump([data], f, ensure_ascii=False)
+        i += 1
+
+
+def get_youku_list():
+    """
+    获取优酷各个频道的地址
+    :return:
+    """
+    channel_list_addr = []
     url = 'http://www.youku.com/'
-    # list = ['tv', 'movie', 'zy', 'music', 'child']
-    url = 'http://movie.youku.com/'
     page = urllib2.urlopen(url)
     soup = BeautifulSoup(page, "html5lib")
-    for info in soup.find_all("a", attrs={'target': "video"}):
-        title = info.get('title')
-        if title is None:
-            pass
-        else:
-            href = info.get('href')
-            # print title + "   " + href
-            data = {
-                u'标题': title,
-                u"网址": href,
-            }
-            with open('youku.txt', "a+") as f:
-                f.write('\n')
-                json.dump([data], f, ensure_ascii=False)
+    for link in soup.find_all('ul', 'top-nav-main'):
+        for info in link.find_all('a'):
+            channel = info.text
+            href = str(info.get('href'))
+            # print href
+            if 'http://' in href:
+                pass
+            else:
+                href = href.replace('//', 'http://')
+            channel_list_addr.append(href)
+    return channel_list_addr
 
 
 if __name__ == '__main__':
-    get_youku_href()
+    x = get_youku_list()
+    get_youku_href(x)
