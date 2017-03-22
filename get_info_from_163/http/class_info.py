f37807a8470f89d27c8e341a0b1cb2929aaea2a0
tangtao <rache008@163.com>
2017-03-22 16:53:58 +0800

    增加了新的功能，每个kind中的信息，根据class和category来区分

--- get_info_from_163/http/class_info.py
+++ get_info_from_163/http/class_info.py
@@ -52,16 +52,273 @@ def calculate_kind(kind=1):
         if 'curl' in line:
             url = line.split()[4]
             x = open('./kind_info/cache_service_kind' + str(kind), "a+")
-            x.write(url + '  ')
+            x.write(url + ' ')
             x.close()
         if 'class' in line:
             cache_size_temp = line.split()[2]
             cache_size = int(cache_size_temp.replace("cache_size=", ''))
+            class_kind = line.split()[0]
+            category = line.split()[1]
             total_cache_size += cache_size
             x = open('./kind_info/cache_service_kind' + str(kind), "a+")
-            x.write(str(cache_size) + '\n')
+            x.write(class_kind + ' ' + category + ' ')
+            x.write("cache_size=" + str(cache_size) + '\n')
             x.close()
     x = open('./kind_info/cache_service_kind' + str(kind), "a+")
     x.write(str(total_cache_size) + '\n')
-    x.write('-------------------------------------')
+    x.write('-------------------------------------\n')
     x.close()
+    kind_info_verbose(kind)
+
+
+def kind_info_verbose(kind=1):
+    f = open('./kind_info/cache_service_kind' + str(kind), "a+")
+    lines = f.readlines()
+    httpcache_count = 0
+    videocache_count = 0
+    mobilecache_count = 0
+    httpcache_size_total = 0
+    videocache_size_total = 0
+    mobilecache_size_total = 0
+    http_other = 0
+    http_other_size_total = 0
+    http_soft = 0
+    http_soft_size_total = 0
+    http_zip = 0
+    http_zip_size_total = 0
+    http_doc = 0
+    http_doc_size_total = 0
+    http_pic = 0
+    http_pic_size_total = 0
+    mobile_android = 0
+    mobile_android_size_total = 0
+    mobile_ios = 0
+    mobile_ios_size_total = 0
+    mobile_wp = 0
+    mobile_wp_size_total = 0
+    video_other = 0
+    video_other_size_total = 0
+    video_youku = 0
+    video_youku_size_total = 0
+    video_miaopai = 0
+    video_miaopai_size_total = 0
+    video_sohu = 0
+    video_sohu_size_total = 0
+    video_fenghuang = 0
+    video_fenghuang_size_total = 0
+    video_netease = 0
+    video_netease_size_total = 0
+    video_ads = 0
+    video_ads_size_total = 0
+    video_56 = 0
+    video_56_size_total = 0
+    video_pptv = 0
+    video_pptv_size_total = 0
+    video_cctv = 0
+    video_cctv_size_total = 0
+    video_bilibili = 0
+    video_bilibili_size_total = 0
+    video_moc = 0
+    video_moc_size_total = 0
+    video_jidong = 0
+    video_jidong_size_total = 0
+    video_tecent = 0
+    video_tecent_size_total = 0
+    video_sina = 0
+    video_sina_size_total = 0
+    video_iqiyi = 0
+    video_iqiyi_size_total = 0
+    video_leshi = 0
+    video_leshi_size_total = 0
+    video_fenxing = 0
+    video_fenxing_size_total = 0
+    video_icntv = 0
+    video_icntv_size_total = 0
+    video_mongotv = 0
+    video_mongotv_size_total = 0
+    for line in lines:
+        if len(line.split()) == 4:
+            if line.split()[1] == "class=httpcache":
+                httpcache_count += 1
+                httpcache_size = int(line.split()[3].replace("cache_size=", ''))
+                httpcache_size_total += httpcache_size
+            elif line.split()[1] == "class=videocache":
+                videocache_count += 1
+                videocache_size = int(line.split()[3].replace("cache_size=", ''))
+                videocache_size_total += videocache_size
+            elif line.split()[1] == "class=mobilecache":
+                mobilecache_count += 1
+                mobilecache_size = int(line.split()[3].replace("cache_size=", ''))
+                mobilecache_size_total += mobilecache_size
+            if line.split()[1] == "class=httpcache" and line.split()[2] == 'category=0':
+                http_other += 1
+                http_other_size = int(line.split()[3].replace("cache_size=", ''))
+                http_other_size_total += http_other_size
+            if line.split()[1] == "class=httpcache" and line.split()[2] == 'category=1':
+                http_soft += 1
+                http_soft_size = int(line.split()[3].replace("cache_size=", ''))
+                http_soft_size_total += http_soft_size
+            if line.split()[1] == "class=httpcache" and line.split()[2] == 'category=2':
+                http_zip += 1
+                http_zip_size = int(line.split()[3].replace("cache_size=", ''))
+                http_zip_size_total += http_zip_size
+            if line.split()[1] == "class=httpcache" and line.split()[2] == 'category=3':
+                http_doc += 1
+                http_doc_size = int(line.split()[3].replace("cache_size=", ''))
+                http_doc_size_total += http_doc_size
+            if line.split()[1] == "class=httpcache" and line.split()[2] == 'category=4':
+                http_pic += 1
+                http_pic_size = int(line.split()[3].replace("cache_size=", ''))
+                http_pic_size_total += http_pic_size
+            if line.split()[1] == "class=mobilecache" and line.split()[2] == 'category=0':
+                mobile_android += 1
+                mobile_android_size = int(line.split()[3].replace("cache_size=", ''))
+                mobile_android_size_total += mobile_android_size
+            if line.split()[1] == "class=mobilecache" and line.split()[2] == 'category=1':
+                mobile_ios += 1
+                mobile_ios_size = int(line.split()[3].replace("cache_size=", ''))
+                mobile_ios_size_total += mobile_ios_size
+            if line.split()[1] == "class=mobilecache" and line.split()[2] == 'category=2':
+                mobile_wp += 1
+                mobile_wp_size = int(line.split()[3].replace("cache_size=", ''))
+                mobile_wp_size_total += mobile_wp_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=0':
+                video_other += 1
+                video_other_size = int(line.split()[3].replace("cache_size=", ''))
+                video_other_size_total += video_other_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=1':
+                video_youku += 1
+                video_youku_size = int(line.split()[3].replace("cache_size=", ''))
+                video_youku_size_total += video_youku_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=2':
+                video_miaopai += 1
+                video_miaopai_size = int(line.split()[3].replace("cache_size=", ''))
+                video_miaopai_size_total += video_miaopai_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=3':
+                video_sohu += 1
+                video_sohu_size = int(line.split()[3].replace("cache_size=", ''))
+                video_sohu_size_total += video_sohu_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=4':
+                video_fenghuang += 1
+                video_fenghuang_size = int(line.split()[3].replace("cache_size=", ''))
+                video_fenghuang_size_total += video_fenghuang_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=5':
+                video_netease += 1
+                video_netease_size = int(line.split()[3].replace("cache_size=", ''))
+                video_netease_size_total += video_netease_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=6':
+                video_ads += 1
+                video_ads_size = int(line.split()[3].replace("cache_size=", ''))
+                video_ads_size_total += video_ads_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=7':
+                video_56 += 1
+                video_56_size = int(line.split()[3].replace("cache_size=", ''))
+                video_56_size_total += video_56_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=8':
+                video_pptv += 1
+                video_pptv_size = int(line.split()[3].replace("cache_size=", ''))
+                video_pptv_size_total += video_pptv_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=9':
+                video_cctv += 1
+                video_cctv_size = int(line.split()[3].replace("cache_size=", ''))
+                video_cctv_size_total += video_cctv_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=10':
+                video_bilibili += 1
+                video_bilibili_size = int(line.split()[3].replace("cache_size=", ''))
+                video_bilibili_size_total += video_bilibili_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=11':
+                video_moc += 1
+                video_moc_size = int(line.split()[3].replace("cache_size=", ''))
+                video_moc_size_total += video_moc_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=12':
+                video_jidong += 1
+                video_jidong_size = int(line.split()[3].replace("cache_size=", ''))
+                video_jidong_size_total += video_jidong_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=13':
+                video_tecent += 1
+                video_tecent_size = int(line.split()[3].replace("cache_size=", ''))
+                video_tecent_size_total += video_tecent_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=14':
+                video_sina += 1
+                video_sina_size = int(line.split()[3].replace("cache_size=", ''))
+                video_sina_size_total += video_sina_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=15':
+                video_iqiyi += 1
+                video_iqiyi_size = int(line.split()[3].replace("cache_size=", ''))
+                video_iqiyi_size_total += video_iqiyi_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=16':
+                video_leshi += 1
+                video_leshi_size = int(line.split()[3].replace("cache_size=", ''))
+                video_leshi_size_total += video_leshi_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=17':
+                video_fenxing += 1
+                video_fenxing_size = int(line.split()[3].replace("cache_size=", ''))
+                video_fenxing_size_total == video_fenxing_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=18':
+                video_icntv += 1
+                video_icntv_size = int(line.split()[3].replace("cache_size=", ''))
+                video_icntv_size_total == video_icntv_size
+            if line.split()[1] == "class=videocache" and line.split()[2] == 'category=19':
+                video_mongotv += 1
+                video_mongotv_size = int(line.split()[3].replace("cache_size=", ''))
+                video_mongotv_size_total += video_mongotv_size
+    f.write('httpcache:' + str(httpcache_count) + ' total_size:' + str(httpcache_size_total) + '\n')
+    f.write('mobilecache:' + str(mobilecache_count) + ' total_size:' + str(mobilecache_size_total) + '\n')
+    f.write('videocache:' + str(videocache_count) + ' total_size:' + str(videocache_size_total) + '\n')
+    f.write('-----------\n')
+    if http_other != 0:
+        f.write('http_other:' + str(http_other) + ' total_size:' + str(http_other_size_total) + '\n')
+    if http_soft != 0:
+        f.write('http_soft:' + str(http_soft) + ' total_size:' + str(http_soft_size_total) + '\n')
+    if http_zip != 0:
+        f.write('http_zip:' + str(http_zip) + ' total_size:' + str(http_zip_size_total) + '\n')
+    if http_doc != 0:
+        f.write('http_doc:' + str(http_doc) + ' total_size:' + str(http_doc_size_total) + '\n')
+    if http_pic != 0:
+        f.write('http_pic:' + str(http_pic) + ' total_size:' + str(http_pic_size_total) + '\n')
+    if mobile_android != 0:
+        f.write('mobile_android:' + str(mobile_android) + ' total_size:' + str(mobile_android_size_total) + '\n')
+    if mobile_ios != 0:
+        f.write('mobile_ios:' + str(mobile_ios) + ' total_size:' + str(mobile_ios_size_total) + '\n')
+    if mobile_wp != 0:
+        f.write('mobile_wp:' + str(mobile_wp) + ' total_size:' + str(mobile_wp_size_total) + '\n')
+    if video_other != 0:
+        f.write('video_other:' + str(video_other) + ' total_size:' + str(video_other_size_total) + '\n')
+    if video_youku != 0:
+        f.write('video_youku:' + str(video_youku) + ' total_size:' + str(video_youku_size_total) + '\n')
+    if video_miaopai != 0:
+        f.write('video_miaopai:' + str(video_miaopai) + ' total_size:' + str(video_miaopai_size_total) + '\n')
+    if video_sohu != 0:
+        f.write('video_sohu:' + str(video_sohu) + ' total_size:' + str(video_sohu_size_total) + '\n')
+    if video_fenghuang != 0:
+        f.write('video_fenghuang:' + str(video_fenghuang) + ' total_size:' + str(video_fenghuang_size_total) + '\n')
+    if video_netease != 0:
+        f.write('video_netease:' + str(video_netease) + ' total_size:' + str(video_netease_size_total) + '\n')
+    if video_ads != 0:
+        f.write('video_ads:' + str(video_ads) + ' total_size:' + str(video_ads_size_total) + '\n')
+    if video_56 != 0:
+        f.write('video_56:' + str(video_56) + ' total_size:' + str(video_56_size_total) + '\n')
+    if video_pptv != 0:
+        f.write('video_pptv:' + str(video_pptv) + ' total_size:' + str(video_pptv_size_total) + '\n')
+    if video_cctv != 0:
+        f.write('video_cctv:' + str(video_cctv) + ' total_size:' + str(video_cctv_size_total) + '\n')
+    if video_bilibili != 0:
+        f.write('video_bilibili:' + str(video_bilibili) + ' total_size:' + str(video_bilibili_size_total) + '\n')
+    if video_moc != 0:
+        f.write('video_moc:' + str(video_moc) + ' total_size:' + str(video_moc_size_total) + '\n')
+    if video_jidong != 0:
+        f.write('video_jidong:' + str(video_jidong) + ' total_size:' + str(video_jidong_size_total) + '\n')
+    if video_tecent != 0:
+        f.write('video_tecent:' + str(video_tecent) + ' total_size:' + str(video_tecent_size_total) + '\n')
+    if video_sina != 0:
+        f.write('video_sina:' + str(video_sina) + ' total_size:' + str(video_sina_size_total) + '\n')
+    if video_iqiyi != 0:
+        f.write('video_iqiyi:' + str(video_iqiyi) + ' total_size:' + str(video_iqiyi_size_total) + '\n')
+    if video_leshi != 0:
+        f.write('video_leshi:' + str(video_leshi) + ' total_size:' + str(video_leshi_size_total) + '\n')
+    if video_fenxing != 0:
+        f.write('video_fenxing:' + str(video_fenxing) + ' total_size:' + str(video_fenxing_size_total) + '\n')
+    if video_icntv != 0:
+        f.write('video_icntv:' + str(video_icntv) + ' total_size:' + str(video_icntv_size_total) + '\n')
+    if video_mongotv != 0:
+        f.write('video_mongotv:' + str(video_mongotv) + ' total_size:' + str(video_mongotv_size_total) + '\n')
--- get_info_from_163/main.py
+++ get_info_from_163/main.py
@@ -41,19 +41,20 @@ def kind1(is_sleep=True, time_line=115):
     """
     # 第一个五分钟 执行如下操作 下载
     print u'执行kind1的上半部分'
-    curl_resource_verbose(0, 2, 5, 'linux', 'windows')
-    curl_resource_verbose(1, 0, 5, 'linux', 'windows')
-    curl_resource_verbose(2, 15, 5, 'linux', 'iphone')
+    curl_resource_verbose(0, 2, 10, 'linux', 'windows')
+    curl_resource_verbose(1, 0, 10, 'linux', 'windows')
+    curl_resource_verbose(2, 15, 10, 'linux', 'iphone')
     if is_sleep:
         print u'间隔125秒后再curl'
         time.sleep(time_line)
     print u'执行kind1的下半部分'
-    curl_resource_verbose(2, 1, 5, 'linux', 'windows')
+    curl_resource_verbose(2, 1, 10, 'linux', 'windows')
+    curl_resource_verbose(1, 0, 10, 'linux', 'windows')
+    curl_resource_verbose(0, 1, 10, 'linux', 'windows')
 
     """
     curl_resource_verbose(0, 2, 5, 'linux', 'windows')
     curl_resource_verbose(0, 4, 5, 'linux', 'windows')
-    curl_resource_verbose(0, 5, 5, 'linux', 'windows')
     curl_resource_verbose(1, 0, 5, 'linux', 'windows')
     curl_resource_verbose(2, 1, 5, 'linux', 'windows')
     curl_resource_verbose(2, 6, 5, 'linux', 'windows')
@@ -67,51 +68,58 @@ def kind1(is_sleep=True, time_line=115):
 
 def kind2(is_sleep=True, time_line=115):
     print u'执行kind2的上半部分'
-    curl_resource_verbose(2, 13, 5, 'linux', 'windows')
-    curl_resource_verbose(0, 4, 5, 'linux', 'iphone')
-    curl_resource_verbose(1, 0, 5, 'linux', 'windows')
+    curl_resource_verbose(2, 13, 10, 'linux', 'windows')
+    curl_resource_verbose(0, 4, 10, 'linux', 'iphone')
+    curl_resource_verbose(1, 0, 10, 'linux', 'windows')
     if is_sleep:
         print u'间隔125秒后再curl'
         time.sleep(time_line)
     print u'执行kind2的下半部分'
-    curl_resource_verbose(2, 16, 5, 'linux', 'windows')
+    curl_resource_verbose(2, 16, 10, 'linux', 'windows')
+    curl_resource_verbose(0, 3, 10, 'linux', 'iphone')
+    curl_resource_verbose(1, 0, 10, 'linux', 'windows')
 
 
 def kind3(is_sleep=True, time_line=115):
     print u'执行kind3的上半部分'
-    curl_resource_verbose(1, 0, 5, 'linux', 'windows')
-    curl_resource_verbose(2, 6, 5, 'linux', 'windows')
-    curl_resource_verbose(0, 3, 5, 'linux', 'windows')
+    curl_resource_verbose(1, 0, 10, 'linux', 'windows')
+    curl_resource_verbose(2, 6, 10, 'linux', 'iphone')
+    curl_resource_verbose(0, 3, 10, 'linux', 'windows')
     if is_sleep:
         print u'间隔125秒后再curl'
         time.sleep(time_line)
     print u'执行kind3的下半部分'
-    curl_resource_verbose(2, 13, 5, 'linux', 'windows')
+    curl_resource_verbose(2, 13, 10, 'linux', 'windows')
+    curl_resource_verbose(1, 0, 10, 'linux', 'windows')
+    curl_resource_verbose(0, 4, 10, 'linux', 'windows')
 
 
 def kind4(is_sleep=True, time_line=115):
     print u'执行kind4的上半部分'
-    curl_resource_verbose(0, 2, 5, 'linux', 'windows')
-    curl_resource_verbose(1, 0, 5, 'linux', 'windows')
-    curl_resource_verbose(2, 1, 5, 'linux', 'windows')
+    curl_resource_verbose(0, 2, 10, 'linux', 'windows')
+    curl_resource_verbose(1, 0, 10, 'linux', 'windows')
+    curl_resource_verbose(2, 1, 10, 'linux', 'iphone')
     if is_sleep:
         print u'间隔125秒后再curl'
         time.sleep(time_line)
     print u'执行kind4的下半部分'
-    curl_resource_verbose(2, 6, 5, 'linux', 'windows')
+    curl_resource_verbose(2, 6, 10, 'linux', 'windows')
+    curl_resource_verbose(0, 3, 10, 'linux', 'windows')
+    curl_resource_verbose(1, 0, 10, 'linux', 'windows')
 
 
 def kind5(is_sleep=True, time_line=115):
     print u'执行kind5的上半部分'
-    curl_resource_verbose(2, 8, 5, 'linux', 'windows')
-    curl_resource_verbose(1, 0, 5, 'linux', 'windows')
-    curl_resource_verbose(0, 4, 5, 'linux', 'windows')
+    curl_resource_verbose(2, 8, 10, 'linux', 'iphone')
+    curl_resource_verbose(1, 0, 10, 'linux', 'windows')
+    curl_resource_verbose(0, 4, 10, 'linux', 'windows')
     if is_sleep:
         print u'间隔125秒后再curl'
         time.sleep(time_line)
     print u'执行kind5的下半部分'
-    curl_resource_verbose(2, 1, 5, 'linux', 'windows')
-    curl_resource_verbose(2, 6, 5, 'linux', 'windows')
+    curl_resource_verbose(2, 1, 10, 'linux', 'windows')
+    curl_resource_verbose(1, 0, 10, 'linux', 'windows')
+    curl_resource_verbose(0, 1, 10, 'linux', 'windows')
 
 
 def timer(expect_start_time='2017-03-21 17:04:00', expect_end_time='2017-03-21 17:20:00'):
@@ -126,6 +134,8 @@ def timer(expect_start_time='2017-03-21 17:04:00', expect_end_time='2017-03-21 1
     expect_end_time = expect_end_time + datetime.timedelta(minutes=5)
     node = 1
     print u'debug信息如下所示：'
+    f = open('linux_curl_log', 'w+')
+    f.close()
     init_debug_info = connect_linux(
         ' /home/icache/icached debug', '192.168.1.106')
     f = open('linux_curl_log', 'a')
@@ -207,7 +217,7 @@ def main():
         i += 1
     print u"准备工作就绪 开始进行curl操作"
     print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    timer('2017-03-22 11:46:00', '2017-03-22 13:25:00')
+    # timer('2017-03-22 13:50:00', '2017-03-22 19:25:00')
 
 
 if __name__ == '__main__':
@@ -215,3 +225,4 @@ if __name__ == '__main__':
     所有的curl指令，因为涉及到相对路径，请都在main这个页面下执行
     """
     main()
+    # kind_info_verbose()
