1b257e31b67847c218e2855e4ca80e350bdeb664
tangtao <rache008@163.com>
2017-03-31 18:26:29 +0800

    成功测试完成，今晚通宵测试验证下。不出意外的话，该工具完成。

--- get_info_from_163/main.py
+++ get_info_from_163/main.py
@@ -23,13 +23,12 @@ I love animals. They taste delicious.
 ┗┻┛  ┗┻┛
 """
 import datetime
-import shutil
 import sys
 from get_info_from_163.http.kind_info import calculate_kind
 from get_info_from_163.http.get_cache import get_all_cache
 from get_info_from_163.tools.del_log import del_all_log
 from get_info_from_163.tools.log.operation_log import my_log, modify_my_log_file_path
-from get_info_from_163.tools.resource_list import get_all_hot_list, hot_list
+from get_info_from_163.tools.resource_list import get_all_hot_list
 from get_info_from_163.tools.timer import timer_customize
 from get_info_from_163.tools.wrong_statistics_by_judge import wrong_statistics_log
 
@@ -42,7 +41,7 @@ def main_kind():
     filepath = './operation_log/' + timestamp
     modify_my_log_file_path(filepath)
     log = my_log()
-    log.info(u'全场关键字 timestamp为：'+timestamp)
+    log.info(u'全场关键字 timestamp为：' + timestamp)
     log.info(u'获取全部资源放入到指定的文件夹中')
     get_all_cache(timestamp)  # 获取全部资源放入到指定的文件夹中
     log.info(u"现在的时间戳节点为：" + timestamp)
@@ -53,7 +52,7 @@ def main_kind():
         i += 1
     log.info(u'准备工作就绪，现在可以开始进行真正的curl操作')
     log.info(u'当前的时间为' + unicode(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-    timer_customize(timestamp, '2017-03-31 16:10:00', '2017-03-31 20:00:00')
+    timer_customize(timestamp, '2017-04-01 00:00:00', '2017-04-01 08:00:00')
     get_all_hot_list(timestamp)
     wrong_statistics_log(timestamp)
     log.info(u"执行完成")
