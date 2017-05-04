a56fae5d6f4012134b19fc9b9a5f8530f347cb0c
tangtao <rache008@163.com>
2017-05-04 18:43:38 +0800

    明天计划 要完成正则匹配的表达式

---
+++ curl_simulate/tools/modify_cache_info.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+#name   = modify_cache_info
+#author = tangtao
+#time   = 2017/5/4 18:24
+#Description=添加描述信息
+#eMail   =tangtao@lhtangtao.com
+#git     =lhtangtao
+# code is far away from bugs with the god animal protecting
+I love animals. They taste delicious.
+┏┓      ┏┓
+┏┛┻━━━┛┻┓
+┃      ☃      ┃
+┃  ┳┛  ┗┳  ┃
+┃      ┻      ┃
+┗━┓      ┏━┛
+┃      ┗━━━┓
+┃  神兽保佑    ┣┓
+┃　永无BUG！   ┏┛
+┗┓┓┏━┳┓┏┛
+┃┫┫  ┃┫┫
+┗┻┛  ┗┻┛
+"""
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
